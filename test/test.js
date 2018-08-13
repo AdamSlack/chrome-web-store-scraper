c7f3da042a9be0bb48eb5ae4c287e8c467c5dce6
adam slack <adamslack@outlook.com>
2018-08-13 11:23:17 +0100

    constructor test

--- test/test.js
+++ test/test.js
@@ -1,11 +1,12 @@
-const scraper = require('../src/chrome-web-store-scraper');
+const ChromeWebStoreScraper = require('../src/chrome-web-store-scraper');
 
 var assert = require('assert');
 
 describe('Scraper Constructor', function() {
-  describe('#constructor()', function() {
-    it('Scraper should have constructeed.', function() {
-      assert.equal(scraper.constructed);
+    describe('#constructor()', function() {
+        it('Scraper should have constructed correctly', function() {
+            const scraper = new ChromeWebStoreScraper();
+            assert.equal(scraper.constructed);
+        });
     });
-  });
 });
