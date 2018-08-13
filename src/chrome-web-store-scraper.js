f8a685e3519a4d54af7b0194440661f982ee372d
adam slack <adamslack@outlook.com>
2018-08-13 11:25:27 +0100

    need to determine what i'm scraping

--- src/chrome-web-store-scraper.js
+++ src/chrome-web-store-scraper.js
@@ -5,6 +5,22 @@ class ChromeWebStoreScraper {
     constructor() {
         this.constructed = true;
     }
+
+    async scrapeOverview() {
+
+    }
+
+    async scrapeReviews() {
+
+    }
+
+    async scrapeSupport() {
+
+    }
+
+    async scrapeRelated() {
+
+    }
 }
 
 module.exports = ChromeWebStoreScraper;
