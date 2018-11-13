19e82c228c5d26d1c4bb11de1c14138827ee7c86
Adam Slack <adamslack@outlook.com>
2018-11-13 21:44:23 +0000

    Page update fixes (#9)
    * Need to handle selecting things using selenium a little better.
    
    * Updated the scraping methods so that it correctly scrapes page data for a given app

--- main.js
+++ main.js
@@ -4,17 +4,18 @@ const scraper = new ChromeWebScraper();
 const fs = require('fs');
 
 async function main() {
-    scraper.search('scraper',{scrollAttempts:200,locale:'da'}).then(
-        (res) => console.log(res[0]),
-        (err) => console.log(err)
-    );
-    
-    //scraper.scrapeApp('https://chrome.google.com/webstore/detail/vidiq-vision-for-youtube/pachckjkecffpdphbpmfolblodfkgbhl')
-    // scraper.scrapeApp('https://chrome.google.com/webstore/detail/scraper-crawler-v3/kbhidgghgflkbalnkoeokcipocmigkfh')
-    // .then(
-    //     (res) => fs.writeFile(`${res.header.title.replace(' ', '_')}_scraping.json`,JSON.stringify(res,null,2),() => console.log('Output Saved')),
+    // scraper.search('scraper',{scrollAttempts:200,locale:'da'}).then(
+    //     (res) => console.log(res[0]),
     //     (err) => console.log(err)
-    // )
+    // );
+    
+    // scraper.scrapeApp('https://chrome.google.com/webstore/detail/vidiq-vision-for-youtube/pachckjkecffpdphbpmfolblodfkgbhl')
+    scraper.scrapeApp('https://chrome.google.com/webstore/detail/scraper-crawler-v3/kbhidgghgflkbalnkoeokcipocmigkfh')
+    .then(
+        // (res) => fs.writeFile(`${res.header.title.replace(' ', '_')}_scraping.json`,JSON.stringify(res,null,2),() => console.log('Output Saved')),
+        (res) => console.log(res),
+        (err) => console.log(err)
+    )
 }
 
 main()
